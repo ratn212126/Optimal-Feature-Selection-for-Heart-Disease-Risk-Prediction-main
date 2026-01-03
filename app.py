8da3d3a03fc7ae26b2204c7d4a55b590f903b700
Ratn Kumar Sharma <ratnkumar2220@gmail.com>
2026-01-03 13:23:17 +0530

    Final

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
 import joblib
-import os
 
 app = Flask(__name__)
 
-MODEL_PATH = "models/full_features/rf_model.pkl"
-model = joblib.load(MODEL_PATH)
+model = joblib.load("models/full_features/rf_model.pkl")
 
 @app.route("/")
 def home():
@@ -13,9 +11,6 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.json["features"] 
+    data = request.json["features"]
     prediction = model.predict([data])
     return jsonify({"prediction": int(prediction[0])})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
